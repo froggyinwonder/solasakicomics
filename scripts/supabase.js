7ade3908d2bfe1dc087572993d55beeda62d5903
froggyinwonder <froggyinwonder@gmail.com>
2025-12-03 21:58:31 +0530

    Delete scripts/supabase.js

--- scripts/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-  "https://rgxtvwnfqglsxnmvkkus.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJneHR2d25mcWdsc3hubXZra3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NjEwNzksImV4cCI6MjA4MDMzNzA3OX0.twtdhIMF4xnaBQsird8CyEb3g3Kh8bI5REChohdyGwo"
-);
